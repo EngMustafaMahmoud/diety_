91e3d90950ec7aecd00c01636d705e8cacac0b9a
Mohamedrifky1 <132143317+Mohamedrifky1@users.noreply.github.com>
2024-04-20 19:27:45 +0200

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-// ignore_for_file: avoid_print
-
 import 'package:diety_/features/Home/Home.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'features/Asks/Gender.dart';
 import 'features/Auth/Login.dart';
 import 'features/Auth/SignUp.dart';
